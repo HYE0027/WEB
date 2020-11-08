ab55d44c48000fbbc40f871868cc05ab76a185a7
HYE0027 <47324711+HYE0027@users.noreply.github.com>
2020-11-09 02:33:42 +0900

    Add files via upload

---
+++ script/sequence.js
@@ -0,0 +1,64 @@
+// Sequence 1
+var imgNum = 0;
+var img = new Image();
+var canvas = document.getElementById('screen1');
+var ctx = canvas.getContext('2d');
+
+img.src = "./img/main_img/main (1).png";
+
+playSequence();
+
+function playSequence() {
+    var timer = setInterval(function() {
+        if (imgNum > 750) {
+            imgNum = 0;
+        }
+        
+        player(imgNum);
+        imgNum++;
+    }, 30);
+}
+
+function player(num) {
+    img.src = "./img/main_img/main (" + num + ").png";
+}
+
+img.addEventListener('load' ,function(e) {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.drawImage(img, 0, 0);
+});
+
+// Sequence 2
+var imgNum2 = 0;
+var img2 = new Image();
+var canvas2 = document.getElementById('screen1');
+var ctx2 = canvas2.getContext('2d');
+var scrollYPos;
+
+img2.src = "./img/sequence/main (1).png";
+
+window.addEventListener('scroll', function(e) {
+    scrollYPos = Math.round(window.scrollY/18);
+    
+    // play once
+    if (scrollYPos == 1) scrollYPos = 1;
+    if (scrollYPos > 240) scrollYPos = 240;
+    
+    // play continuously
+    scrollYPos = scrollYPos % 240;
+    player2(scrollYPos);
+})
+
+function player2(num) {
+    img2.src = "./img/sequence/main (" + num + ").png";
+}
+
+img2.addEventListener('load' ,function(e) {
+    ctx2.clearRect(0, 0, ctx2.canvas.width, ctx2.canvas.height);
+    ctx2.drawImage(img2, 0, 0);
+});
+
+
+
+
+
